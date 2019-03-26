8ca60f91abaa19d2141fa5c0bab93fb7216f7cdc
CheshireRat <nuzhin.ivan@gmail.com>
2019-03-26 23:30:22 +0200

    add screenshoot for login test if test failed

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.PropertyReader;
 
+import java.util.concurrent.TimeUnit;
+
 public class DriverManager {
     private static WebDriver driver = null;
     private static WebDriverWait wait = null;
@@ -12,7 +14,7 @@ public class DriverManager {
     public static void initDriver() {
         System.setProperty("webdriver.chrome.driver", PropertyReader.readValue("chromedriverPath"));
         driver = new ChromeDriver();
-
+        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS); //TODO - ask google how to do it correctly
     }
     public static WebDriver getDriver() {
        return driver ;
@@ -20,7 +22,9 @@ public class DriverManager {
 
     public static WebDriverWait waiting() {
         WebDriverWait wait = new WebDriverWait(driver, 20);
+
         return wait ;
+
     }
 
 }
